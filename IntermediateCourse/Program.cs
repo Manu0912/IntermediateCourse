aea7f3f3ff9fbb8b286f945ac338dcac8371f968
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-07 18:28:41 -0700

    Stopwatch exercise

--- IntermediateCourse/Program.cs
+++ IntermediateCourse/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntermediateCourse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,7 +9,27 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StopWatch Stopwatch = new();
+            string flag = "s";
+            int option = 0;
+
+            while(flag == "s")
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("Do you want to start Or Stop? 0/1");
+                    option = Int32.Parse(Console.ReadLine());
+
+                    switch (option) { 
+                        case 0: Stopwatch.Start(); break;
+                        case 1: Console.WriteLine(Stopwatch.Stop()); break;
+                        default: return;
+                    }
+                }
+                Console.WriteLine("Do you want to continue?");
+                flag = Console.ReadLine();
+            }
+            
         }
     }
 }
---
+++ IntermediateCourse/StopWatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntermediateCourse
+{
+    public class StopWatch
+    {
+        private TimeSpan Timespan;
+        private DateTime InitialDate;
+        private DateTime FinalDate;
+        private bool Stopped;
+
+        public StopWatch()
+        {
+            Stopped = true;
+        }
+
+        public void Start()
+        {
+            if (Stopped)
+            {
+                InitialDate = DateTime.Now;
+                Stopped = false;   
+            }
+            else
+            {
+                throw new InvalidOperationException("First you have to stop the StopWatch");
+            }
+        }
+
+        public TimeSpan Stop()
+        {
+            if (!Stopped)
+            {
+                FinalDate = DateTime.Now;
+                Timespan = FinalDate - InitialDate;
+                Stopped = true;
+                return Timespan;
+            }
+            else
+            {
+                throw new InvalidOperationException("First you have to start the StopWatch");
+            }
+            
+        }
+    }
+}
