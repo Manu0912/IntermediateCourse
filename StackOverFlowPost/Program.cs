7a5b29843bda892ad770dff32094a7b8b96fa56d
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-08 10:25:02 -0700

    Exercise StackOverflow

--- StackOverFlowPost/Program.cs
+++ StackOverFlowPost/Program.cs
@@ -1,4 +1,4 @@
-﻿using IntermediateCourse;
+﻿using StackOverFlow;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +9,31 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
+            StackOverflowPost stackOverflowPost = new();
+            int option = 0;
+            string flag = "s";
 
+            while(flag == "s")
+            {
+                Console.WriteLine("Like or Dislike? 0/1");
+                option = Int32.Parse(Console.ReadLine());
+                if (option == 0)
+                {
+                    stackOverflowPost.GiveLike();
+                }
+                else
+                {
+                    stackOverflowPost.GiveDislike();
+                }
+
+                Console.WriteLine("Continue? s/n");
+                flag = Console.ReadLine();
+            }
+
+            Console.WriteLine("Likes: " + stackOverflowPost.Likes);
+            Console.WriteLine("Dislikes: " + stackOverflowPost.Dislikes);
+            
         }
+
     }
 }
---
+++ StackOverFlowPost/StackOverflowPost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverFlow
+{
+    public class StackOverflowPost
+    {
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private DateTime CreationDate;
+        public int Likes { get; private set; }
+        public int Dislikes { get; private set; }
+
+        public StackOverflowPost()
+        {
+            CreationDate = DateTime.Now;
+            Likes = 0;
+            Dislikes = 0;
+        }
+
+        public StackOverflowPost(string title, string description)
+        {
+            CreationDate = DateTime.Now;
+            Title = title;
+            Description = description;
+            Likes = 0;
+            Dislikes = 0;
+        }
+
+        public void GiveLike()
+        {
+            Likes++;
+        }
+
+        public void GiveDislike()
+        {
+            Dislikes++;
+        }
+
+    }
+}
