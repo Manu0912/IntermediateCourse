05e5f47e6fb2730bd09d84e46016a8fbfa8567d0
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-08 12:00:10 -0700

    Stack exercise

--- IntermediateCourse/Program.cs
+++ IntermediateCourse/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using StackNamespace;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -8,7 +9,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
---
+++ IntermediateCourse/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackNamespace
+{
+    public class Stack
+    {
+        public List<object> List { get; set; }
+
+        public Stack()
+        {
+            List = new();
+        }
+
+        public void Push(object Obj)
+        {
+            if (Obj != null)
+            {
+                List.Add(Obj);
+            }
+            else
+            {
+                throw new InvalidOperationException("The object cannot be null");
+            }
+        }
+
+        public object Pop()
+        {
+            if (List.Count > 0)
+            {
+                var obj = List[List.Count - 1];
+                List.Remove(obj);
+                return obj;
+            }
+            else
+            {
+                throw new InvalidOperationException("The list cannot be null");
+            }
+        }
+
+        public void Clear()
+        {
+            List.Clear();
+        }
+    }
+}
